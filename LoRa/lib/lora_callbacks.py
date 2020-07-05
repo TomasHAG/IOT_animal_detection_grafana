261d1f2246f6a66186b152548f7c0f19b983992d
TomasHAG <42994176+TomasHAG@users.noreply.github.com>
2020-07-05 16:14:03 +0200

    Delete lora_callbacks.py

--- LoRa/lib/lora_callbacks.py
+++
@@ -1,8 +0,0 @@
-def lora_cb(lora): # initilize logic for callback to get and send packages.
-    events = lora.events()
-
-    if events & LoRa.TX_PACKET_EVENT: # send package with trigger
-        light_manager.data_send()
-
-# activate callbacks
-lora.callback(trigger=(LoRa.TX_PACKET_EVENT), handler=lora_cb)
